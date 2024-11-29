53e49e7df5adbf54b95581129357919947399b85
MRoiban <roiban.marius.alexandru+github@gmail.com>
2024-11-29 13:56:10 +0100

    Refactor transition and learning functions in ValueIteration class

--- src/rl/value_iteration.py
+++ src/rl/value_iteration.py
@@ -2,6 +2,7 @@ import lle
 import numpy as np
 from .env import Labyrinth
 from collections import defaultdict
+import random
 class ValueIteration:
     """
     Value Iteration algorithm for solving a reinforcement learning environment.
@@ -31,17 +32,105 @@ class ValueIteration:
         self.transitions = {}
 
 
+    # def transition(self, state, action, update=False):
+    #     reward = self.env.step(action)
+    #     s_prime = self.env.get_observation()
+        
+    #     self.env.step(lle.Action(action).opposite().value)  # Reset to original state
+        
+    #     if update:
+    #         # Learning the Transitions
+    #         if (state, action) not in self.transitions:
+    #             self.transitions[(state, action)] = []
+            
+    #         # Update or add new transition
+    #         transition_found = False
+    #         for transition in self.transitions[(state, action)]:
+    #             if transition[0] == s_prime:
+    #                 transition[1] += 1
+    #                 transition_found = True
+    #                 break
+            
+    #         if not transition_found:
+    #             self.transitions[(state, action)].append([s_prime, 1, 0])
+            
+    #         # Update probabilities for all transitions from this state-action pair
+    #         total_visits = 0
+    #         for _, visit_count, _ in self.transitions[(state, action)]:
+    #             total_visits += visit_count
+    #         for transition in self.transitions[(state, action)]:
+    #             transition[2] = transition[1] / total_visits  # Update probability
+            
+            
+    #         # Learning the Rewards
+    #         if (state, action, s_prime) not in self.rewards:
+    #             self.rewards[(state, action, s_prime)] = [0, 0, 0]
+            
+    #         self.rewards[(state, action, s_prime)][0] += reward
+    #         self.rewards[(state, action, s_prime)][1] += 1
+    #         self.rewards[(state, action, s_prime)][2] = (
+    #             self.rewards[(state, action, s_prime)][0] / 
+    #             self.rewards[(state, action, s_prime)][1]
+    #         )
+        
+    #     # Reading Values - Handle case when transition hasn't been seen
+    #     probability = 0
+    #     if (state, action) in self.transitions:
+    #         for transition in self.transitions[(state, action)]:
+    #             if transition[0] == s_prime:
+    #                 probability = transition[2]
+    #                 break
+        
+    #     reward_value = 0
+    #     if (state, action, s_prime) in self.rewards:
+    #         reward_value = self.rewards[(state, action, s_prime)][2]
+    #     else:
+    #         reward_value = reward  # Use immediate reward if no history
+
     def transition(self, state, action, update=False):
         reward = self.env.step(action)
         s_prime = self.env.get_observation()
-        
-        self.env.step(lle.Action(action).opposite().value)  # Reset to original state
-        
-        if update:
-            # Learning the Transitions
+        probability = 0
+        if (state, action) in self.transitions:
+                for transition in self.transitions[(state, action)]:
+                    if transition[0] == s_prime:
+                        probability = transition[2]
+                        break
+            
+        reward_value = 0
+        if (state, action, s_prime) in self.rewards:
+            reward_value = self.rewards[(state, action, s_prime)][2]
+        else:
+            reward_value = reward  # Use immediate reward if no history
+        return s_prime, probability, reward_value
+
+
+    def learn_transitions(self, n_samples: int):
+        """
+        Learn the transition function by interacting with the environment.
+
+        Parameters:
+        - n_samples (int): The number of state-action pairs to sample and update.
+        """
+        for _ in range(n_samples):
+            # Randomly sample a valid state and action
+            state = self.env.get_valid_states()[random.randint(0, len(self.env.get_valid_states()) - 1)]
+            self.env.set_state(state)
+            
+            action = self.env.get_all_actions()[random.randint(0, len(self.env.get_all_actions()) - 1)]
+
+            # Take the action and observe the next state and reward
+            reward = self.env.step(action)
+            s_prime = self.env.get_observation()
+
+
+            # Reset the environment to the original state
+            self.env.reset()
+
+            # Update transition counts and probabilities
             if (state, action) not in self.transitions:
                 self.transitions[(state, action)] = []
-            
+
             # Update or add new transition
             transition_found = False
             for transition in self.transitions[(state, action)]:
@@ -49,51 +138,31 @@ class ValueIteration:
                     transition[1] += 1
                     transition_found = True
                     break
-            
+
             if not transition_found:
                 self.transitions[(state, action)].append([s_prime, 1, 0])
-            
+
             # Update probabilities for all transitions from this state-action pair
-            total_visits = 0
-            for _, visit_count, _ in self.transitions[(state, action)]:
-                total_visits += visit_count
+            total_visits = sum(t[1] for t in self.transitions[(state, action)])
             for transition in self.transitions[(state, action)]:
                 transition[2] = transition[1] / total_visits  # Update probability
-            
-            
-            # Learning the Rewards
+
+            # Update the reward table
             if (state, action, s_prime) not in self.rewards:
                 self.rewards[(state, action, s_prime)] = [0, 0, 0]
-            
+
             self.rewards[(state, action, s_prime)][0] += reward
             self.rewards[(state, action, s_prime)][1] += 1
             self.rewards[(state, action, s_prime)][2] = (
                 self.rewards[(state, action, s_prime)][0] / 
                 self.rewards[(state, action, s_prime)][1]
             )
-        
-        # Reading Values - Handle case when transition hasn't been seen
-        probability = 0
-        if (state, action) in self.transitions:
-            for transition in self.transitions[(state, action)]:
-                if transition[0] == s_prime:
-                    probability = transition[2]
-                    break
-        
-        reward_value = 0
-        if (state, action, s_prime) in self.rewards:
-            reward_value = self.rewards[(state, action, s_prime)][2]
-        else:
-            reward_value = reward  # Use immediate reward if no history
-        
-        return s_prime, probability, reward_value
-
 
     def bellman_equation(self, state) -> float:
         values = np.zeros(len(self.env.get_all_actions()))
         for action in self.env.get_all_actions():
             s_prime, probability, reward = self.transition(state, action, True)
-            values[action] += probability * (reward + (self.gamma ** self.step_count) * self.value_table[s_prime])
+            values[action] += probability * (reward + (self.gamma) * self.value_table[s_prime])
         return max(values)
             
 
@@ -104,15 +173,19 @@ class ValueIteration:
         Parameters:
         - n_updates (int): The total number of updates to perform
         """
+        self.learn_transitions(1_000_000)
         for _ in range(n_updates):
             delta = 0
+            updated_values = np.zeros(self.env_size)
             for state in self.env.get_valid_states():
                 value = self.value_table[state]
-                self.value_table[state] = self.bellman_equation(state)
-                delta = max(delta, abs(value - self.value_table[state]))
+                updated_value = self.bellman_equation(state)
+                updated_values[state] = updated_value
+                delta = max(delta, abs(value - updated_value))
                 self.step_count += 1
                 if delta < self.theta:
                     break
+            self.value_table = updated_values
 
 
 
